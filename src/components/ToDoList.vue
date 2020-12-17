626cea72ee26b0672d2cdf89d7f15ab823a66115
Diego <diegotozetto@gmail.com>
2020-12-16 22:27:29 -0300

    Adiciona conteúdo base no componente 'ToDoList'

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    ToDoList
+  <div class="container mt-4">
+    <div class="jumbotron">
+      <h1 class="display-4">To-Do List</h1>
+      <p class="lead">Libere espaço na sua mente. Recupere a clareza e a tranquilidade, tirando todas essas tarefas da sua cabeça e colocando na sua lista de tarefas.</p>
+      <hr/>
+      <router-link class="btn btn-secondary btn-sm mb-4" to="/task" role="button">+ Nova Tarefa</router-link>
+    </div>
   </div>
 </template>
 
