108bc2fb01565153f1095d02e09a75a974fc8ff9
Diego <diegotozetto@gmail.com>
2020-12-17 23:49:18 -0300

    Adiciona sistema de validação do formulário

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -6,32 +6,44 @@
         </div>
         <div class="card-body">
           <h6>Nome da Tarefa</h6>
-          <form @submit.prevent="formSubmitHandler">
-            <input
-              autoFocus
-              type="text"
-              class="form-control"
-              id="text"
-              name="text"
-              placeholder="Informe o nome da tarefa..."
-              :value="text"
-              @input="text = $event.target.value"
-            />
-            <router-link class="btn btn-secondary btn-sm mt-2 mr-2" to="/home" role="button">Cancelar</router-link>
-            <button
-              type="submit"
-              class="btn btn-primary btn-sm mt-2"
-            >
-              Salvar
-            </button>
-          </form>
+            <form @submit.prevent="formSubmitHandler">
+              <validation-provider
+                rules="required|max:100"
+                v-slot="{errors}"
+              >
+                <input
+                  autoFocus
+                  type="text"
+                  class="form-control"
+                  id="text"
+                  name="text"
+                  placeholder="Informe o nome da tarefa..."
+                  :value="text"
+                  @input="text = $event.target.value"
+                  :class="errors[0] ? 'invalid' : null"
+                />
+                <div class="error">{{errors[0]}}</div>
+              </validation-provider>
+              <router-link class="btn btn-secondary btn-sm mt-2 mr-2" to="/home" role="button">Cancelar</router-link>
+              <button
+                type="submit"
+                class="btn btn-primary btn-sm mt-2"
+              >
+                Salvar
+              </button>
+            </form>
         </div>
       </div>
     </div>
 </template>
 
 <script>
+  import { ValidationProvider } from 'vee-validate'
+
   export default {
+    components: {
+      ValidationProvider
+    },
     data() {
       return {
         text: '',
@@ -39,26 +51,40 @@
     },
     methods: {
       async formSubmitHandler() {
-        const json = {text: this.text}
+        var currentText = this.text.trim();
 
-        const response = await fetch(
-          'https://web-api-todolist.herokuapp.com/tasks/',
-          {
-            method: "POST",
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(json)
-          }
-        );
-        const data = await response.json();
-        alert(data.message);
+        if(currentText.length == 0 || currentText.length > 100)
+          alert("Preencha o formulário corretamente.");
+        else {
+          const json = {text: currentText}
 
-        if (response.status == 201)
-          this.$router.push('/home');
+          const response = await fetch(
+            'https://web-api-todolist.herokuapp.com/tasks/',
+            {
+              method: "POST",
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify(json)
+            }
+          );
+          const data = await response.json();
+          alert(data.message);
+
+          if (response.status == 201)
+            this.$router.push('/home');
+        }
       }
     }
   }
 </script>
 
-<style lang="stylus" scoped>
+<style scoped>
+  .error {
+    color: red;
+    font-size: 0.8em;
+    margin-left: 0.4em;
+  }
 
+  input.invalid {
+    border: 1px solid red
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -10,6 +10,20 @@ Vue.config.productionTip = false
 
 import { routes } from './routes';
 
+import { extend } from 'vee-validate';
+
+import { required } from 'vee-validate/dist/rules';
+extend('required', {
+  ...required,
+  message: 'O nome da tarefa é um campo obrigatório.'
+});
+
+import { max } from 'vee-validate/dist/rules';
+extend('max', {
+  ...max,
+  message: (campo, regra) => `O nome da tarefa deve possuir no máximo ${regra.length} caracteres.`
+});
+
 const router = new VueRouter({
   routes: routes,
   mode: 'history'
