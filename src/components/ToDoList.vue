dc6a046b4ebd0c5cd6459d96acb5302977f95c97
Diego <diegotozetto@gmail.com>
2020-12-17 23:58:21 -0300

    Modifica cor do botão 'Nova Tarefa'

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -4,7 +4,7 @@
       <h1 class="display-4">To-Do List</h1>
       <p class="lead">Libere espaço na sua mente. Recupere a clareza e a tranquilidade, tirando todas essas tarefas da sua cabeça e colocando na sua lista de tarefas.</p>
       <hr/>
-      <router-link class="btn btn-secondary btn-sm mb-4" to="/task" role="button">+ Nova Tarefa</router-link>
+      <router-link class="btn btn-primary btn-sm mb-4" to="/task" role="button">+ Nova Tarefa</router-link>
       <TaskList/>
     </div>
 
