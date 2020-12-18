3a5797abf91763043ac93b2df8f18262e73203b0
Diego <diegotozetto@gmail.com>
2020-12-18 18:31:38 -0300

    Ajuste nas configurações de deploy

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const app = express();
 
-const app = express()
+app.use(favicon(__dirname + '/dist/favicon.ico'));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
-app.get(/.*/, function (req, res) {
-	res.sendFile(path.join(__dirname, '/dist/index.html'))
-})
-
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta: ${port}`)
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log("Servidor Rodando...");
 });
